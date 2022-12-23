9899ab613aa6b80540171a7282ba8ea201356ba6
Rylei Ellis <77601329+crpkry13i@users.noreply.github.com>
2022-12-23 21:00:00 +0000

    added search visible function

--- backend/routes/api/items.js
+++ backend/routes/api/items.js
@@ -54,7 +54,7 @@ router.get("/", auth.optional, function(req, res, next) {
   }
 
   if(typeof req.query.title !== "undefined") {
-    const regex = new RegExp(req.query.title);
+    const regex = new RegExp(req.query.title, "i");
     query.title = { $regex: regex };
   }
 
